6f1c606753739f747fdb30acd35a04e8e4d6a2fe
Chris Bibby <cmbibby@gmail.com>
2022-03-03 09:56:51 +1030

    debug api

---
+++ inc/Offer.php
@@ -0,0 +1,5 @@
+<?php
+namespace LA_App_Sync;
+Class Offer{
+
+}
--- inc/Sync.php
+++ inc/Sync.php
@@ -10,10 +10,25 @@ Class Sync{
 
 	public function get_offers(){
 		$offers = array();
-		echo 'yep we go get oofers';
+		echo 'Off to get some Offers <br /><br /><hr />';
 		$response = wp_remote_retrieve_body(wp_remote_get('https://app.localadvantage.com.au/api/v2/offers'));
-		$response = json_decode($response,true);
-		$offers[] = $response;
+		$response = json_decode($response, true);
+		// echo '<pre>' . var_export( $response['offers'], true ) . '</pre>';
+		$offer_count = 0;
+		foreach($response['offers'] as $offer){
+				$offer_count ++;
+				echo $offer['active'] . '<br />';
+				echo $offer['vendor_name'] . '<br />';
+				echo $offer['offer_title'] . '<br />';
+				echo $offer['offer_description'] . '<br />';
+				echo $offer['offer_conditions'] . '<br />';
+				echo $offer['vendor_about'] . '<br />';
+				echo $offer['address'] . '<br />';
+				echo $offer['phone'] . '<br />';
+				echo $offer['website'] . '<br />';
+				echo '<hr />';
+		}
+		echo $offer_count;
 
 	}
 }
