48c84ccacacc39d6b7966b6a761806bb188022ed
Chris Bibby <cmbibby@gmail.com>
2022-03-12 22:33:32 +1030

    dynamic post type

--- inc/Sync.php
+++ inc/Sync.php
@@ -22,104 +22,114 @@ class Sync
 
 
 		$offers = array();
-		echo 'Off to get some Offers <br /><br /><hr />';
 		$response = wp_remote_retrieve_body(wp_remote_get('https://app.localadvantage.com.au/api/v2/offers'));
 		$response = json_decode($response, true);
 		$offer_count = 0;
 		foreach ($response['offers'] as $offer) {
-			if($offer_count > 10){
+			// if($offer_count > 10){
+			// 	return;
+			// }
+
+			switch ($offer['region_id']) {
+				case 1:
+					$post_type = 'sw_offers';
+					$location_type = 'sw_location';
+					$category_type = 'sw_category';
+					break;
+				case 2:
+					$post_type = 'gs_offers';
+					$location_type = 'gs_location';
+					$category_type = 'gs_category';
+			}
+
+			$offer_count++;
+
+			// Locations
+
+			$location_term = term_exists($offer['location_name'], $location_type);
+
+			if (!$location_term) {
+				$location_term = wp_insert_term($offer['location_name'], $location_type);
+			}
+
+			// Categories
+
+			$categories = $offer['categories'];
+
+			$categories_to_add = [];
+			foreach ($categories as $category) {
+				$existing_category = term_exists($category['name'], $category_type);
+				if ($existing_category) {
+					$categories_to_add[] = $existing_category['term_taxonomy_id'];
+				} else {
+					$new_category = wp_insert_term($category['name'], $category_type);
+					$categories_to_add[] = $new_category['term_taxonomy_id'];
+				}
+			}
+
+			// Check if the post exists and if so delete it
+
+			$existing_post_id = post_exists($offer['vendor_name'], '', '', $post_type, 'publish');
+
+			if ($existing_post_id > 0) {
+				wp_delete_post($existing_post_id);
+			}
+
+			// Check the active field and bail if it isn't there
+
+			if (false == $offer['active']) {
 				return;
 			}
 
-			if (1 == $offer['active'] && 1 == $offer['region_id']) {
-				$offer_count++;
+			// Lets insert the post
 
-				// Locations
+			$offer_id = wp_insert_post(array(
+				'post_name' => sanitize_title($offer['vendor_name']),
+				'post_title' => $offer['vendor_name'],
+				'post_type' => $post_type,
+				'post_status' => 'publish',
+				'tax_input' => array(
+					$location_type => $location_term,
+					$category_type => $categories_to_add
+				)
+			));
 
-				$location_term = term_exists($offer['location_name'], 'sw_location');
+			// Update the custom fields
 
-				if(! $location_term){
-					$location_term = wp_insert_term($offer['location_name'], 'sw_location');
-				}
-
-				// Categories
-
-
-
-				$categories = $offer['categories'];
-
-				$categories_to_add = [];
-				foreach($categories as $category){
-					$existing_category = term_exists($category['name'],'sw_category');
-					if($existing_category){
-						$categories_to_add[] = $existing_category['term_taxonomy_id'];
-					}else{
-						$new_category = wp_insert_term($category['name'], 'sw_category');
-						$categories_to_add[] = $new_category['term_taxonomy_id'];
-					}
-				}
-
-				// Check if the post exists and if so delete it
-
-				$existing_post_id = post_exists($offer['vendor_name'],'','','sw_offers','publish');
-
-				echo '<pre>' . var_export( $existing_post_id, true ) . '</pre>';
-
-				if($existing_post_id > 0){
-					wp_delete_post($existing_post_id);
-				}
-
-				// Lets insert the post
-
-				$offer_id = wp_insert_post(array(
-					'post_name' => sanitize_title($offer['vendor_name']),
-					'post_title' => $offer['vendor_name'],
-					'post_type' => 'sw_offers',
-					'post_status' => 'publish',
-					'tax_input' => array(
-						'sw_location' => $location_term,
-						'sw_category' => $categories_to_add
-					)
-				));
-
-				$acf_fields = array(
-					'field_59a55be66555f' => 'offer_title',
-					'field_59a55c1365560' => 'offer_description',
-					'field_621f64e9b0172' => 'offer_conditions',
-					'field_59a55c8365561' => 'vendor',
-					'field_59a55deb8384e' => 'address',
-					'field_59a67fa63fbb0' => 'phone',
-					'field_59a55e0b11ceb' => 'website',
-					'field_6228978384673' => 'latitude',
-					'field_6228978abb5b6' => 'longitude'
-				);
-				foreach ($acf_fields as $key => $name) {
-					update_field($key, $offer[$name], $offer_id);
-				}
-
-				// Do the Logo
-
-				$images = $offer['images'];
-
-				$logo = array_shift($images);
-
-				$media_id = media_sideload_image('https://app.localadvantage.com.au/images/catalog/' . $logo, null, $offer['vendor_name'], 'id');
-
-				set_post_thumbnail($offer_id, $media_id);
-
-				// Do the Gallery images
-
-				$gallery_media = array();
-				foreach($images as $image){
-					$image_name = Utilities::format_image_title($offer['vendor_name'], $image);
-					$gallery_media[] = media_sideload_image('https://app.localadvantage.com.au/images/catalog/'. $image, null, $image_name, 'id');
-				}
-
-				update_field('field_59a74fe116594', $gallery_media, $offer_id);
-
-
-
+			$acf_fields = array(
+				'field_59a55be66555f' => 'offer_title',
+				'field_59a55c1365560' => 'offer_description',
+				'field_621f64e9b0172' => 'offer_conditions',
+				'field_59a55c8365561' => 'vendor',
+				'field_59a55deb8384e' => 'address',
+				'field_59a67fa63fbb0' => 'phone',
+				'field_59a55e0b11ceb' => 'website',
+				'field_6228978384673' => 'latitude',
+				'field_6228978abb5b6' => 'longitude'
+			);
+			foreach ($acf_fields as $key => $name) {
+				update_field($key, $offer[$name], $offer_id);
 			}
+
+			// Do the Logo
+
+			$images = $offer['images'];
+
+			$logo = array_shift($images);
+
+			$media_id = media_sideload_image('https://app.localadvantage.com.au/images/catalog/' . $logo, null, $offer['vendor_name'], 'id');
+
+			set_post_thumbnail($offer_id, $media_id);
+
+			// Do the Gallery images
+
+			$gallery_media = array();
+			foreach ($images as $image) {
+				$image_name = Utilities::format_image_title($offer['vendor_name'], $image);
+				$gallery_media[] = media_sideload_image('https://app.localadvantage.com.au/images/catalog/' . $image, null, $image_name, 'id');
+			}
+
+			update_field('field_59a74fe116594', $gallery_media, $offer_id);
 		}
 		echo $offer_count;
 	}
