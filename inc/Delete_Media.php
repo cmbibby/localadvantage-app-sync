af8c01ed8425c52333d23b86e819aa110e0331a9
Chris Bibby <cmbibby@gmail.com>
2022-03-13 00:08:45 +1030

    delete media

--- inc/Delete_Media.php
+++ inc/Delete_Media.php
@@ -7,15 +7,21 @@ class Delete_Media
 	public function __construct()
 	{
 
-		add_action('before_delete_post', array($this, 'delete_post_attachments'));
+		add_action('before_delete_post', array($this, 'delete_post_attachments'),10,2);
 	}
 
 
-	public function delete_post_attachments($post_id)
+	public function delete_post_attachments($post_id, $post)
 	{
-		$attachments = get_attached_media( '', $post_id );
-		foreach ($attachments as $attachment) {
-		  wp_delete_attachment( $attachment->ID, 'true' );
+		if($post->post_type !== 'sw_offers'){
+			return;
+		}
+
+		$logo_id = get_post_thumbnail_id($post);
+		wp_delete_attachment($logo_id, true);
+		$gallery_attachments = get_field('offer_gallery', $post_id);
+		foreach ($gallery_attachments as $attachment) {
+		  wp_delete_attachment( $attachment['id'], true );
 		}
 	}
 }
