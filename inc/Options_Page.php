8954cd3f117f8e4b1b5b546c34c3bc1afbcceaae
Chris Bibby <cmbibby@gmail.com>
2022-03-14 00:20:36 +1030

    style admin page

--- inc/Options_Page.php
+++ inc/Options_Page.php
@@ -9,6 +9,7 @@ class Options_Page {
 
 
 
+
 	function __construct() {
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 	}
@@ -19,22 +20,34 @@ class Options_Page {
 	}
 
 
-	public function page_content() {             ?>
+	public function page_content() {                 ?>
 		<h1>App Sync</h1>
 		<h2>Last Update Time : <span id="updateTime"><?php echo get_field( 'last_update_time', 'option' ); ?></span></h2>
-		<p id="infoContainer"></p>
+
 		<div class="button-container">
 			<div class="all-offers">
-				<button id="import_all" class="button-primary">Import All Offers</button>
-				<div id="allOffersSpinner" class="spinner" style="float:none;width:auto;height:auto;padding:10px 0 10px 50px;background-position:20px 0;"></div>
+				<div class="card">
+					<h2 class="title">Import All Offers</h2>
+					<p>
+						This will import all offers from the API. Please note this will take a long time and depding on available server resources may fail.
+					</p>
+					<button id="import_all" class="button-primary">Import All Offers</button>
+					<div id="allOffersSpinner" class="spinner" style="float:none;width:auto;height:auto;padding:10px 0 10px 50px;background-position:20px 0;"></div>
+				</div>
 			</div>
 			<div class="lastest-offers">
-				<button id="import_latest" class="button-primary">Import Latest Offers</button>
-				<div id="latestOffersSpinner" class="spinner" style="float:none;width:auto;height:auto;padding:10px 0 10px 50px;background-position:20px 0;"></div>
+				<div class="card">
+					<h2 class="title">Import Latest Offers</h2>
+					<p>Fetch the offers that have been updated since the last update time.</p>
+					<button id="import_latest" class="button-primary">Import Latest Offers</button>
+					<div id="latestOffersSpinner" class="spinner" style="float:none;width:auto;height:auto;padding:10px 0 10px 50px;background-position:20px 0;"></div>
+				</div>
+
 			</div>
 		</div>
+		<h2 id="infoContainer"></h2>
 
 
-	<?php
+<?php
 	}
 }
