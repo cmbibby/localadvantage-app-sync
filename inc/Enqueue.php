8068da01754ddd1be116dd37267e6a32d64fe0dd
Chris Bibby <cmbibby@gmail.com>
2022-03-14 00:13:19 +1030

    add some css

--- inc/Enqueue.php
+++ inc/Enqueue.php
@@ -1,18 +1,18 @@
 <?php
 namespace LA_App_Sync;
-Class Enqueue{
-	public function __construct()
-	{
-		// Hook up the js
+class Enqueue {
+	public function __construct() {
+		 // Hook up the js
 
-		add_action('admin_enqueue_scripts', array($this,'action_enqueue_js'));
+		add_action( 'admin_enqueue_scripts', array( $this, 'action_enqueue_js' ) );
 	}
 
-	public function action_enqueue_js($hook){
-		if ('tools_page_la_app_sync' !== $hook){
+	public function action_enqueue_js( $hook ) {
+		if ( 'tools_page_la_app_sync' !== $hook ) {
 			return;
 		}
 
-		wp_enqueue_script('la-app-sync', Plugin::$plugin_dir_url . 'js/la-app-sync.js', ['wp-util']);
+		wp_enqueue_script( 'la-app-sync-js', Plugin::$plugin_dir_url . 'js/la-app-sync.js', [ 'wp-util' ] );
+		wp_enqueue_style( 'la-app-sync-js', Plugin::$plugin_dir_url . 'css/la-app-sync.css' );
 	}
 }
--- inc/Options_Page.php
+++ inc/Options_Page.php
@@ -8,6 +8,7 @@ class Options_Page {
 
 
 
+
 	function __construct() {
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 	}
@@ -18,14 +19,20 @@ class Options_Page {
 	}
 
 
-	public function page_content() {         ?>
+	public function page_content() {             ?>
 		<h1>App Sync</h1>
 		<h2>Last Update Time : <span id="updateTime"><?php echo get_field( 'last_update_time', 'option' ); ?></span></h2>
 		<p id="infoContainer"></p>
-
-		<button id="import_all" class="button-primary">Import All Offers</button>
-		<div id="allOffersSpinner" class="spinner" style="float:none;width:auto;height:auto;padding:10px 0 10px 50px;background-position:20px 0;">
-
+		<div class="button-container">
+			<div class="all-offers">
+				<button id="import_all" class="button-primary">Import All Offers</button>
+				<div id="allOffersSpinner" class="spinner" style="float:none;width:auto;height:auto;padding:10px 0 10px 50px;background-position:20px 0;"></div>
+			</div>
+			<div class="lastest-offers">
+				<button id="import_latest" class="button-primary">Import Latest Offers</button>
+				<div id="latestOffersSpinner" class="spinner" style="float:none;width:auto;height:auto;padding:10px 0 10px 50px;background-position:20px 0;"></div>
+			</div>
+		</div>
 
 
 	<?php
